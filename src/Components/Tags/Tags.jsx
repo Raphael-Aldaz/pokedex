45780b9cee7b4b95d08c26c53b4b84b4cd2c1ee8
Raphael-Aldaz <raphael.aldaz@gmail.com>
2022-10-12 21:36:53 +0200

    'test'

---
+++ src/Components/Tags/Tags.jsx
@@ -0,0 +1,11 @@
+import {theme} from '../../Theme/Theme'
+const Tags = ({name})=> {
+    const style = {
+        backgroundColor: `${theme.colors.backgroundTypes[name]}`
+    }
+    console.log(theme.colors.backgroundTypes[name],'color')
+    return(
+        <p style={style}>{name} / </p>
+    )
+}
+export default Tags
